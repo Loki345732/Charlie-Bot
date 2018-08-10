7e47da1c45e58a6ebbd8966f5bce7d5a22e49fc2
Loki345732 <40841404+Loki345732@users.noreply.github.com>
2018-08-09 20:56:37 -0400

    Update bot.js

--- bot.js
+++ bot.js
@@ -4,7 +4,7 @@ client.on('ready', () => {
     console.log('I am ready!');
 });
 client.on('message', message => {
-    if (message.author.id !== '450759594751426580'|| message.author.id== '361013294783528962' || message.author.id !== '450834805072855040'|| message.author.id !== '464251263257935872') {return;}
+    if (message.author.id !== '450759594751426580' || message.author.id !== '361013294783528962' || message.author.id !== '450834805072855040' || message.author.id !== '464251263257935872') {return;}
     if (message.author.id === '464251263257935872') {return;}
     if (message.content.toLowerCase() === 'ring ring') 
        {message.channel.send('You have a bad connection');return;}
